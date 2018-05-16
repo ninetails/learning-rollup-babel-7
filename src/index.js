3267cbf49573078b533cf5d9dfa1383487730483
Carlos Kazuo <ninetails.nine@gmail.com>
2018-05-16 03:34:43 -0300

    Added simple examples in esnext

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-import '@babel/polyfill'
-import foo from './foo'
+import foo from './foo.js'
 
-foo()
+const obj = {
+  nested: {
+    example: {
+      foo: 'bar'
+    }
+  }
+}
+
+const test = obj.bla ?.foo ?.wee ?? obj.nested?.example?.foo;
+
+const asyncAdd = (number) => Promise.resolve(number + 5);
+
+const split = (sep, str) =>
+  str.split(sep)
+
+(async () => {
+  const hw = 'Hello'
+    |> (x => `${x} world`)
+    |> (x => `${x}! ${test}`)
+    |> (_ => split(' ', _))
+
+  foo(hw)
+})();
